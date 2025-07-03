8d0747532f646e9c76c30ef61be3eead14813447
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-03 15:36:03 +0200

    [upd] bonus 4.

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -31,10 +31,11 @@ for (let i = teachers.length - 1; i >= 0; i--) {
 }
 console.log(teachers);
 
-
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
